a577c99b4af24625dee17b9e07d66bb4f2841274
Tom Charnock <tom@charnock.fr>
2025-03-28 17:46:48 +0100

    Hardwiring the dtb location

--- starfive/visionfive/v2/uboot.nix
+++ starfive/visionfive/v2/uboot.nix
@@ -9,6 +9,10 @@ buildUBoot {
 
   defconfig = "starfive_visionfive2_defconfig";
 
+  extraConfig = ''
+    CONFIG_DEFAULT_FDT_FILE=starfive/jh7110-starfive-visionfive-2-v1.3b.dtb
+  '';
+
   filesToInstall = [
     "spl/u-boot-spl.bin.normal.out"
     "u-boot.itb"
